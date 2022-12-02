92360f878ae890a273a8c442442714d083482fa2
haris <harisasrofi14@gmail.com>
2022-12-02 15:06:13 +0700

    first commit

---
+++ IOSFundamental/IOSFundamentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSFundamentalApp.swift
+//  IOSFundamental
+//
+//  Created by Haris Asrofi on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct IOSFundamentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
